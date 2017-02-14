9b6366a429b1a37a099800f9d435b6076f00836b
supernoir <kawohl.bentodesign@gmail.com>
2017-02-14 20:28:01 +0100

    let there be light

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+          loaders: ["style-loader","css-loader","sass-loader"],
+          test: /\.scss$/
+      }
+    ]
+  }
+}
